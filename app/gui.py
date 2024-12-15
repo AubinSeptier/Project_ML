1b11e914f4e2e46539c184ef419a63ec1e05a6f4
Aubin Septier <aubinseptier@gmail.com>
2024-12-15 16:38:41 +0100

    update app ui and readme

--- app/gui.py
+++ app/gui.py
@@ -1,15 +1,22 @@
 import customtkinter as ctk
 import tkinter as tk
 from predict import Predictor
-import pandas as pd
 
 class App(ctk.CTk):
     def __init__(self):
+        """
+        Initialise l'application et son interface graphique et définit les propriétés de la fenêtre principale.
+        """
         super().__init__()
         self.title("Health Disease Prediction App")
         self.geometry("1280x720")
         ctk.set_appearance_mode("dark")
-        ctk.set_default_color_theme("blue")
+        ctk.set_default_color_theme("green")
+        self.resizable(True, True)
+        self.minsize(800, 720)
+        
+        for i in range(4):
+            self.grid_columnconfigure(i, weight=1)
         
         
         self.appTitle = ctk.CTkLabel(self, text="Heart Disease Prediction App", font=("Arial", 30, "bold"))
@@ -25,90 +32,115 @@ class App(ctk.CTk):
         self.ageLabel = ctk.CTkLabel(self, text="Age")
         self.ageLabel.grid(row=2, column=0, padx=10, pady=10, sticky="w")
         self.ageEntry = ctk.CTkEntry(self, placeholder_text="30")
-        self.ageEntry.grid(row=2, column=1, padx=10, pady=10, sticky="w")
+        self.ageEntry.grid(row=2, column=1, padx=10, pady=10, sticky="ew")
         
         self.heightLabel = ctk.CTkLabel(self, text="Height (cm)")
         self.heightLabel.grid(row=3, column=0, padx=10, pady=10, sticky="w")
         self.heightEntry = ctk.CTkEntry(self, placeholder_text="180")
-        self.heightEntry.grid(row=3, column=1, padx=10, pady=10, sticky="w")
+        self.heightEntry.grid(row=3, column=1, padx=10, pady=10, sticky="ew")
         
         self.weightLabel = ctk.CTkLabel(self, text="Weight (kg)")
         self.weightLabel.grid(row=4, column=0, padx=10, pady=10, sticky="w")
         self.weightEntry = ctk.CTkEntry(self, placeholder_text="70")
-        self.weightEntry.grid(row=4, column=1, padx=10, pady=10, sticky="w")
+        self.weightEntry.grid(row=4, column=1, padx=10, pady=10, sticky="ew")
         
         self.genderVar = tk.IntVar(value=1)
         self.genderLabel = ctk.CTkLabel(self, text="Gender")
         self.genderLabel.grid(row=5, column=0, padx=10, pady=10, sticky="w")
         self.genderWomanRadio = ctk.CTkRadioButton(self, text="Woman", variable=self.genderVar, value=1)
-        self.genderWomanRadio.grid(row=5, column=1, padx=10, pady=10, sticky="w")
+        self.genderWomanRadio.grid(row=5, column=1, padx=10, pady=10, sticky="ew")
         self.genderManRadio = ctk.CTkRadioButton(self, text="Man", variable=self.genderVar, value=2)
-        self.genderManRadio.grid(row=5, column=2, padx=10, pady=10, sticky="w")
+        self.genderManRadio.grid(row=5, column=2, padx=10, pady=10, sticky="ew")
         
         
         self.systolicLabel = ctk.CTkLabel(self, text="Systolic Blood Pressure (mmHg)")
         self.systolicLabel.grid(row=6, column=0, padx=10, pady=10, sticky="w")
         self.systolicEntry = ctk.CTkEntry(self, placeholder_text="120")
-        self.systolicEntry.grid(row=6, column=1, padx=10, pady=10, sticky="w")
+        self.systolicEntry.grid(row=6, column=1, padx=10, pady=10, sticky="ew")
         
         self.diastolicLabel = ctk.CTkLabel(self, text="Diastolic Blood Pressure (mmHg)")
         self.diastolicLabel.grid(row=6, column=2, padx=10, pady=10, sticky="w")
         self.diastolicEntry = ctk.CTkEntry(self, placeholder_text="80")
-        self.diastolicEntry.grid(row=6, column=3, padx=10, pady=10, sticky="w")
+        self.diastolicEntry.grid(row=6, column=3, padx=10, pady=10, sticky="ew")
         
         self.cholVar = tk.IntVar(value=1)
         self.cholLabel = ctk.CTkLabel(self, text="Cholesterol")
         self.cholLabel.grid(row=7, column=0, padx=10, pady=10, sticky="w")
         self.cholNormalRadio = ctk.CTkRadioButton(self, text="Normal", variable=self.cholVar, value=1)
-        self.cholNormalRadio.grid(row=7, column=1, padx=10, pady=10, sticky="w")
+        self.cholNormalRadio.grid(row=7, column=1, padx=10, pady=10, sticky="ew")
         self.cholAboveNormalRadio = ctk.CTkRadioButton(self, text="Above Normal", variable=self.cholVar, value=2)
-        self.cholAboveNormalRadio.grid(row=7, column=2, padx=10, pady=10, sticky="w")
+        self.cholAboveNormalRadio.grid(row=7, column=2, padx=10, pady=10, sticky="ew")
         self.cholWellAboveRadio = ctk.CTkRadioButton(self, text="Well Above Normal", variable=self.cholVar, value=3)
-        self.cholWellAboveRadio.grid(row=7, column=3, padx=10, pady=10, sticky="w")
+        self.cholWellAboveRadio.grid(row=7, column=3, padx=10, pady=10, sticky="ew")
         
         self.glucVar = tk.IntVar(value=1)
         self.glucLabel = ctk.CTkLabel(self, text="Glucose")
         self.glucLabel.grid(row=8, column=0, padx=10, pady=10, sticky="w")
         self.glucNormalRadio = ctk.CTkRadioButton(self, text="Normal", variable=self.glucVar, value=1)
-        self.glucNormalRadio.grid(row=8, column=1, padx=10, pady=10, sticky="w")
+        self.glucNormalRadio.grid(row=8, column=1, padx=10, pady=10, sticky="ew")
         self.glucAboveNormalRadio = ctk.CTkRadioButton(self, text="Above Normal", variable=self.glucVar, value=2)
-        self.glucAboveNormalRadio.grid(row=8, column=2, padx=10, pady=10, sticky="w")
+        self.glucAboveNormalRadio.grid(row=8, column=2, padx=10, pady=10, sticky="ew")
         self.glucWellAboveRadio = ctk.CTkRadioButton(self, text="Well Above Normal", variable=self.glucVar, value=3)
-        self.glucWellAboveRadio.grid(row=8, column=3, padx=10, pady=10, sticky="w")
+        self.glucWellAboveRadio.grid(row=8, column=3, padx=10, pady=10, sticky="ew")
         
         self.smokeVar = tk.IntVar(value=0)
         self.smokeLabel = ctk.CTkLabel(self, text="Smoking")
         self.smokeLabel.grid(row=9, column=0, padx=10, pady=10, sticky="w")
         self.smokeRadio = ctk.CTkRadioButton(self, text="No", variable=self.smokeVar, value=0)
-        self.smokeRadio.grid(row=9, column=1, padx=10, pady=10, sticky="w")
+        self.smokeRadio.grid(row=9, column=1, padx=10, pady=10, sticky="ew")
         self.smokeRadio = ctk.CTkRadioButton(self, text="Yes", variable=self.smokeVar, value=1)
-        self.smokeRadio.grid(row=9, column=2, padx=10, pady=10, sticky="w")
+        self.smokeRadio.grid(row=9, column=2, padx=10, pady=10, sticky="ew")
         
         self.alcoVar = tk.IntVar(value=0)
         self.alcoLabel = ctk.CTkLabel(self, text="Alcohol")
         self.alcoLabel.grid(row=10, column=0, padx=10, pady=10, sticky="w")
         self.alcoRadio = ctk.CTkRadioButton(self, text="No", variable=self.alcoVar, value=0)
-        self.alcoRadio.grid(row=10, column=1, padx=10, pady=10, sticky="w")
+        self.alcoRadio.grid(row=10, column=1, padx=10, pady=10, sticky="ew")
         self.alcoRadio = ctk.CTkRadioButton(self, text="Yes", variable=self.alcoVar, value=1)
-        self.alcoRadio.grid(row=10, column=2, padx=10, pady=10, sticky="w")
+        self.alcoRadio.grid(row=10, column=2, padx=10, pady=10, sticky="ew")
         
         self.activeVar = tk.IntVar(value=0)
         self.activeLabel = ctk.CTkLabel(self, text="Physical Activity")
         self.activeLabel.grid(row=11, column=0, padx=10, pady=10, sticky="w")
         self.activeRadio = ctk.CTkRadioButton(self, text="No", variable=self.activeVar, value=0)
-        self.activeRadio.grid(row=11, column=1, padx=10, pady=10, sticky="w")
+        self.activeRadio.grid(row=11, column=1, padx=10, pady=10, sticky="ew")
         self.activeRadio = ctk.CTkRadioButton(self, text="Yes", variable=self.activeVar, value=1)
-        self.activeRadio.grid(row=11, column=2, padx=10, pady=10, sticky="w")
+        self.activeRadio.grid(row=11, column=2, padx=10, pady=10, sticky="ew")
         
-        self.predictButton = ctk.CTkButton(self, text="Predict", command=self.predict)
+        self.predictButton = ctk.CTkButton(self, corner_radius=4, text="Predict", command=self.predict)
         self.predictButton.grid(row=12, column=1, columnspan=2, padx=10, pady=10, sticky="ew")
         
         self.predictionLabel = ctk.CTkLabel(self, text="", font=("Arial", 15, "bold"))
         self.predictionLabel.grid(row=13, column=0, columnspan=4, padx=10, pady=10, sticky="ew")
+        
+        self.warningsLabel = ctk.CTkLabel(self, text="This application is not a substitute for proper medical tests and advice.", font=("Arial", 10))
+        self.warningsLabel.grid(row=14, column=0, columnspan=4, padx=10, pady=10, sticky="ew")
     
         
         
     def predict(self):
+        """
+        Prévoit la probabilité d'être atteint d'une maladie cardiaque à partir des données utilisateur fournies.
+        La prédiction est réalisée à l'aide du modèle spécifié par l'utilisateur.
+        Si la prédiction est 0, cela signifie qu'aucune maladie cardiaque n'est détectée. La probabilité d'avoir une maladie cardiaque est donnée.
+        Si la prédiction est 1, cela signifie qu'une maladie cardiaque est détectée.
+        La probabilité de la prédiction est affichée.
+        
+        Vérifie que les données utilisateurs fournies sont du bon type et non vides.
+        
+        Les données utilisateur fournies sont:
+            - age (en années)
+            - gender
+            - height (en cm)
+            - weight (en kg)
+            - systolic blood pressure (en mmHg)
+            - diastolic blood pressure (en mmHg)
+            - cholesterol level (taux de cholestérol)
+            - glucose level (taux de glucose)
+            - smoking (fumeur ou non) 
+            - alcohol consumption (consommation d'alcool ou non)
+            - physical activity (pratique d'activité physique ou non)
+        """
         raw_age = self.ageEntry.get()
         raw_height = self.heightEntry.get()
         raw_weight = self.weightEntry.get()
@@ -152,12 +184,24 @@ class App(ctk.CTk):
         else:
             text = "Heart Disease Detected (probability: {:.2f})".format(proba[0][1])
             self.predictionLabel.configure(text=text, text_color="red")
-        
-        print("Updated")
+            
         del predictor      
         
         
     def check_values(self, raw_age, raw_height, raw_weight, raw_systolic_bp, raw_diastolic_bp):
+        """
+        Vérifie que les valeurs fournies sont correctes (plausibles et non vides) et les convertit dans le bon type.
+        
+        Args:
+            raw_age (str): âge de l'utilisateur (en années)
+            raw_height (str): taille de l'utilisateur (en cm)
+            raw_weight (str): poids de l'utilisateur (en kg)
+            raw_systolic_bp (str): pression artérielle systolique de l'utilisateur (en mmHg)
+            raw_diastolic_bp (str): pression artérielle diastolique de l'utilisateur (en mmHg)
+        
+        Returns:
+            tuple: un tuple contenant l'âge, la taille, le poids, la pression artérielle systolique et diastolique de l'utilisateur dans le bon type
+        """
         try:
             age = int(raw_age)
             height = int(raw_height)
